177f534cd9b67fc4001cf31fbd516a3fa1066478
Oleg Kishinsky <kishinsky_oleg@softforsports.com>
2025-09-02 09:00:19 +0300

    add htmx

--- internal/view/layout/base_templ.go
+++ internal/view/layout/base_templ.go
@@ -62,7 +62,7 @@ func Base(props BaseProps) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</div></body></head></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</div><script src=\"/assets/main.js\"></script></body></head></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
